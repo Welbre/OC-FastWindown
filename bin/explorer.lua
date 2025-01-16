05d992ee99bb153e39b8f814bc8d1af0b6a8f939
Welber <welbersrze@gmail.com>
2025-01-16 13:15:53 -0300

    Create a file explorer

--- bin/explorer.lua
+++ bin/explorer.lua
@@ -2,4 +2,6 @@ local fwind = require("fwind")
 local filesystem = require("filesystem")
 local shell = require("shell")
 
-local screen_idx = fwind
+local screen_resolution = {fwind.fdraw.gpu}
+
+local screen_idx = fwind.Application:new(x, y, width, height)
